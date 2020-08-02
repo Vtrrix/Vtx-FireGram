9dca76ae67da0528398db769b9ecefc963987132
VIVEK SONY <45513498+Vtrrix@users.noreply.github.com>
2020-08-02 21:50:55 +0530

    Updates

---
+++ precache-manifest.320f442d98873118c5a83217a4b58ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169365778e20b02c11ee0649fb33e39c",
+    "url": "/Vtx-FireGram/index.html"
+  },
+  {
+    "revision": "cf9714ec9c3854b28964",
+    "url": "/Vtx-FireGram/static/css/main.2dd894f1.chunk.css"
+  },
+  {
+    "revision": "ee243750f889acc4c5a3",
+    "url": "/Vtx-FireGram/static/js/2.1442d3b8.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/Vtx-FireGram/static/js/2.1442d3b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9714ec9c3854b28964",
+    "url": "/Vtx-FireGram/static/js/main.0bb9b4d3.chunk.js"
+  },
+  {
+    "revision": "c4d2f558dbb2ce9a8b32",
+    "url": "/Vtx-FireGram/static/js/runtime-main.ab745c9d.js"
+  }
+]);
